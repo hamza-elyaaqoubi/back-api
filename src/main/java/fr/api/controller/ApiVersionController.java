830897c2bb7aa87f8fd85614193d390ca198fc57
hel <elyaaqoubi.hamza@gmail.com>
2017-03-26 23:13:33 +0200

    cross origin

--- src/main/java/fr/api/controller/ApiVersionController.java
+++ src/main/java/fr/api/controller/ApiVersionController.java
@@ -3,12 +3,14 @@ package fr.api.controller;
 import fr.api.bean.Version;
 import fr.api.service.ApiVersionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/back-api")
 @RestController
+@CrossOrigin(maxAge = 1800)
 public class ApiVersionController {
 
     @Autowired
